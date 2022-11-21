b31a6745c6246d970050e7f3c28777312fed2603
Samudrala Neha <samudralaneha14@gmail.com>
2022-11-21 20:10:35 +0530

    First commit to Maven Java Project 66A8

---
+++ src/main/java/SEGroup/MavenJavaProject66A8/App.java
@@ -0,0 +1,13 @@
+package SEGroup.MavenJavaProject66A8;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "This is First Maven project in Eclipse" );
+    }
+}
